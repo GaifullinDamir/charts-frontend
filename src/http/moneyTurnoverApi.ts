260e1f18940af90f2d10d2e8deacd48e98c80790
Damir <gaifullindr@gmail.com>
2024-08-23 13:24:56 +0300

    Реализовал получение данных с сервера

--- src/http/moneyTurnoverApi.ts
+++ src/http/moneyTurnoverApi.ts
@@ -6,12 +6,16 @@ type PaginationType = {
     page: number;
     size: number;
 }
+
+interface IReponse {
+    data: IMoneyTurnover[];
+}
 export const fetchAllMoneyTurnoverPaginated = createAsyncThunk(
     'moneyTurnover/fetchAll',
     async ({page, size}: PaginationType, thunkApi) => {
         try {
-            const res = await $host.get<IMoneyTurnover[]>(`moneyTurnover?_sort=-date&_page=${page}&_per_page=${size}`);
-            const moneyTurnoverArray = res.data;
+            const res = await $host.get<IReponse>(`moneyTurnover?_sort=-date&_page=${page}&_per_page=${size}`);
+            const moneyTurnoverArray = res.data.data;
             return moneyTurnoverArray;
         } catch (error) {
             return thunkApi.rejectWithValue((error as Error).message);
